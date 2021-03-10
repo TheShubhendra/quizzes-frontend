386fc73db4e50e68ac73097b56292b5bbf8f4815
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-03-10 17:14:24 +0530

    Add browser router

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router} from 'react-router-dom';
+
 if (process.env.NODEENV !== "production"){
     require("dotenv").config();
 }
 ReactDOM.render(
   <React.StrictMode>
+  <Router>
     <App />
+  </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
